0845de14abfd9599ae3c843f02ecfcca98ed6ee5
Mateus <160790022+Mateusssssssssss@users.noreply.github.com>
2024-11-26 09:17:19 -0300

    criado alguns arquivos html que usarei, views e urls criadas
    remedy para resetar o html, e paginas html que serão usadas, views e urls criadas

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 app_name ='blog'
 
 urlpatterns = [
-    path('', views.index, name='index' )
+    path('', views.index, name='index' ),
+    path('page/', views.page, name='page' ),
+    path('page/post/', views.post, name='post' ), 
 ]
